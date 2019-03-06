abeb2c67e40932855711e4fb281ddd141815e0fd
rost rohinevych <rostyslav@strings.com>
2019-03-06 14:07:33 +0200

    indentation fixed

--- Source/ShoutFactory.swift
+++ Source/ShoutFactory.swift
@@ -257,9 +257,9 @@ open class ShoutView: UIView {
       subtitleLabel.sizeToFit()
     } else if panGestureRecognizer.state == .changed {
       panGestureActive = true
-      
-      let maxTranslation = subtitleLabel.bounds.size.height - subtitleLabelOriginalHeight
 
+      let maxTranslation = subtitleLabel.bounds.size.height - subtitleLabelOriginalHeight
+      
       if translation.y >= maxTranslation {
         frame.size.height = internalHeight + maxTranslation
           + (translation.y - maxTranslation) / 25 + Dimensions.touchOffset
@@ -272,7 +272,7 @@ open class ShoutView: UIView {
 
       subtitleLabel.numberOfLines = 2
       subtitleLabel.sizeToFit()
-
+      
       UIView.animate(withDuration: 0.2, animations: {
         self.frame.size.height = height + Dimensions.touchOffset
       }, completion: { _ in
